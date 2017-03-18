78a500a584d55ab41489b8ba94ba38fcc0b9982d
zhanghengbo <hengbo.zhang@best3456.com>
2017-03-18 10:53:39 +0800

    代码更新

--- service-dao/src/test/java/goodsTest.java
+++ service-dao/src/test/java/goodsTest.java
@@ -1,5 +1,9 @@
 import core.admin.dao.GoodsMapper;
+import core.admin.dao.GoodsMessageMapper;
+import core.admin.dao.GoodsPicMapper;
 import core.admin.domain.Goods;
+import core.admin.domain.GoodsMessage;
+import core.admin.domain.GoodsPic;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,6 +20,12 @@ public class goodsTest {
     @Resource
     private GoodsMapper goodsMapper;
 
+    @Resource
+    private GoodsPicMapper goodsPicMapper;
+
+    @Resource
+    private GoodsMessageMapper goodsMessageMapper;
+
     @Test
     public void insertGoods(){
         Goods goods = new Goods();
@@ -30,4 +40,21 @@ public class goodsTest {
         goodsMapper.save(goods);
     }
 
+    @Test
+    public void insertGoodsPic(){
+        GoodsPic goodsPic = new GoodsPic();
+        goodsPic.setGoodsId(0L);
+        goodsPic.setPicURL("/test/test.png");
+        goodsPic.setPicDesc("测试图片描述");
+        goodsPicMapper.save(goodsPic);
+    }
+
+    @Test
+    public void insertGoodsMessage(){
+        GoodsMessage goodsMessage = new GoodsMessage();
+        goodsMessage.setContent("测试评论内容");
+        goodsMessage.setGoodsId(0L);
+        goodsMessage.setUserId(0L);
+        goodsMessageMapper.save(goodsMessage);
+    }
 }
